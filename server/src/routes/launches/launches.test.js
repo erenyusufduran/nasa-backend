97d9d47c581cd2c5285f93606b178cc49dcbf3f9
Eren Yusuf Duran <erenydurann@gmail.com>
2022-04-22 02:34:03 +0300

    get-with-supertest

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -1,14 +1,17 @@
+const request = require("supertest");
+const app = require("../../app");
+
 describe("Test GET /launches", () => {
-  test("It should respond with 200 success", () => {
-    const response = 200;
-    expect(response).toBe(200);
+  test("It should respond with 200 success", async () => {
+    const response = await request(app)
+      .get("/launches")
+      .expect("Content-Type", /json/)
+      .expect(200);
   });
 });
 
-describe("TEST POST /launch",() => {
-    test('It should respond with 200 success',() => {
-        
-    });
-    test('It should catch missing required properties',() => {});
-    test('It should catch invalid dates',() => {});
+describe("TEST POST /launch", () => {
+  test("It should respond with 200 success", () => {});
+  test("It should catch missing required properties", () => {});
+  test("It should catch invalid dates", () => {});
 });
