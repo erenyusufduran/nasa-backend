6ce4eb367299cded42ee8cc0f11fdd972aa3ec5c
Eren Yusuf Duran <erenyusufduran1905@gmail.com>
2022-05-16 13:21:00 +0300

    getting-latest-flight-number

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -3,7 +3,7 @@ const planets = require("./planets.mongo");
 
 const launches = new Map();
 
-let latestFlightNumber = 100;
+const DEFAULT_FLIGHT_NUMBER = 100;
 
 const launch = {
   flightNumber: 100,
@@ -23,6 +23,15 @@ saveLaunch(launch);
 function existsLaunchWithId(launchId) {
   return launches.has(launchId);
 }
+
+async function getLatestFlightNumber() {
+  const latestLaunch = await launchesDatabase.findOne().sort("-flightNumber");
+  // -flightNumber --> descending order..
+  if(!latestLaunch) {
+    return DEFAULT_FLIGHT_NUMBER;
+  }
+  return latestLaunch.flightNumber;
+} 
 
 async function getAllLaunches() {
   return await launchesDatabase.find({}, { _id: 0, __v: 0 });
