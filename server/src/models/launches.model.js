e3c8d5ade092d4548cc04859a323a6504a7b1919
Eren Yusuf Duran <erenyusufduran1905@gmail.com>
2022-05-16 13:56:15 +0300

    mongo-setOnInsert

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -46,7 +46,7 @@ async function saveLaunch(launch) {
     throw new Error("No matching planet found.");
   }
 
-  await launchesDatabase.updateOne(
+  await launchesDatabase.findOneAndUpdate(
     {
       flightNumber: launch.flightNumber,
     },
