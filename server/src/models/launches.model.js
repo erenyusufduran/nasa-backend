b708a2a23f3260f431b49aa479cfaa62c57e9c4c
Eren Yusuf Duran <erenyusufduran1905@gmail.com>
2022-05-27 20:50:58 +0300

    persisting-spaceX-launches-to-database

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -42,6 +42,12 @@ async function populateLaunches() {
       ],
     },
   });
+
+  if (response.status !== 200) {
+    console.log("Problem downloading launch data.");
+    throw new Error("Launch data download failed.");
+  }
+
   const launchDocs = response.data.docs;
   for (const launchDoc of launchDocs) {
     const payloads = launchDoc["payloads"];
@@ -59,7 +65,7 @@ async function populateLaunches() {
       customers,
     };
     console.log(`${launch.flightNumber} ${launch.mission}`);
-    //TODO: populate launches collection...
+    await saveLaunch(launch);
   }
 }
 
@@ -100,14 +106,6 @@ async function getAllLaunches() {
 }
 
 async function saveLaunch(launch) {
-  const planet = await planets.findOne({
-    keplerName: launch.target,
-  });
-
-  if (!planet) {
-    throw new Error("No matching planet found.");
-  }
-
   await launchesDatabase.findOneAndUpdate(
     {
       flightNumber: launch.flightNumber,
@@ -120,6 +118,13 @@ async function saveLaunch(launch) {
 }
 
 async function scheduleNewLaunch(launch) {
+  const planet = await planets.findOne({
+    keplerName: launch.target,
+  });
+
+  if (!planet) {
+    throw new Error("No matching planet found.");
+  }
   const newFlightNumber = (await getLatestFlightNumber()) + 1;
   const newLaunch = Object.assign(launch, {
     success: true,
