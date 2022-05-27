8d778d529732a8c7c61c41cec5c5168b5fe7cb1f
Eren Yusuf Duran <erenyusufduran1905@gmail.com>
2022-05-28 00:40:00 +0300

    dotenv

--- server/src/server.js
+++ server/src/server.js
@@ -1,11 +1,13 @@
 const http = require("http");
+require('dotenv').config();
+
 const app = require("./app");
 
 const { mongoConnect } = require("./services/mongo");
 const { loadPlanetsData } = require("./models/planets.model");
 const { loadLaunchData } = require("./models/launches.model");
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT ;
 
 const server = http.createServer(app);
 
--- server/src/services/mongo.js
+++ server/src/services/mongo.js
@@ -1,7 +1,6 @@
 const mongoose = require("mongoose");
 
-const MONGO_URL =
-  "mongodb+srv://nasa-api:Ex8qUaxFZ57ELsSx@nasacluster.dhuux.mongodb.net/nasa?retryWrites=true&w=majority";
+const MONGO_URL = process.env.MONGO_URL;
 
 mongoose.connection.once("open", () => {
   console.log("MongoDB connection ready!");
